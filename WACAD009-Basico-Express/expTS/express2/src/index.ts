8ebc42486b24f2e6eeee7bfe8e21c587fc9547ae
Breno Uchoa <uchoa.breno2016@gmail.com>
2026-06-09 04:06:05 -0400

    Exercicio express parte 4

--- WACAD009-Basico-Express/expTS/express2/src/index.ts
+++ WACAD009-Basico-Express/expTS/express2/src/index.ts
@@ -1,17 +1,32 @@
 import express from "express";
 
-import { validateEnv } from "./utils/validateEnv";
+import { validateEnv }
+from "./utils/validateEnv";
+
+import { logger }
+from "./middleware/logger";
 
 validateEnv();
 
 const app = express();
 
-const PORT = Number(process.env.PORT);
+app.use(logger);
+
+const PORT =
+    Number(process.env.PORT);
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+
+    res.send(
+        "<h1>Hello World!</h1>"
+    );
+
 });
 
 app.listen(PORT, () => {
-    console.log(`Express app iniciada na porta ${PORT}.`);
+
+    console.log(
+        `Express app iniciada na porta ${PORT}.`
+    );
+
 });
