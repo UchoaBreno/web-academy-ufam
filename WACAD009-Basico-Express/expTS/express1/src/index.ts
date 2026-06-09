d18492137c3bfc36328e1bfd554512fbc748114b
Breno Uchoa <uchoa.breno2016@gmail.com>
2026-06-09 02:05:19 -0400

    Exercicio 2, p1

---
+++ WACAD009-Basico-Express/expTS/express1/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+
+    res.send(`
+        <h1>Hello World!</h1>
+    `);
+
+});
+
+app.listen(3333, () => {
+
+    console.log(
+        "Express app iniciada na porta 3333."
+    );
+
+});
